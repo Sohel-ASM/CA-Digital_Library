bb0dc06ddca60c2f0de503d381f01f7e079a2444
S Bhuiyan <sohelbhuiyan77@gmail.com>
2019-09-11 08:14:29 -0400

    method for add rating in the '_ratings' array

--- main.js
+++ main.js
@@ -23,12 +23,20 @@ class Media {
     toggleCheckOutStatus() {
         this.isCheckedOut = !this.isCheckedOut;
     }
-    // this method returns the average value of the 'ratings' array
+    // this method returns the average value of the 'this._ratings' array
     getAverageRating() {
         let sumOfRatings = this.ratings.reduce((currentSum, rating) => currentSum + rating);
         const lengthOfArray = this.ratings.length;
         let average = sumOfRatings / lengthOfArray;
         return average;
     }
-
+    // method for add rating in the 'this._ratings' array
+    addRating(input) {
+        // to make sure input is a number between 1 and 5 including
+        if (typeof (input) == 'number' && input > 0 && input < 6) {
+            this.ratings.push(input);
+        } else {
+            console.log('Error! Pick a number from 1 to 5.')
+        }
+    }
 }
